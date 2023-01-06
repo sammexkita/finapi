5c4020d17228707006e910a2e1557d902aa6dc80
Kita Igniter <smtecsystem.469@gmail.com>
2023-01-06 19:01:46 -0300

    Withdraw function

--- src/index.js
+++ src/index.js
@@ -19,6 +19,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.use(express.json());
 
 app.post("/account", (req, res) => {
@@ -60,6 +72,27 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
     amount,
     created_at: new Date(),
     type: "credit"
+  };
+
+  customer.statement.push(statemnentOperation);
+
+  return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return res.status(400).json({ error: "Insufficient funds!"});
+  }
+
+  const statemnentOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
   };
 
   customer.statement.push(statemnentOperation);
